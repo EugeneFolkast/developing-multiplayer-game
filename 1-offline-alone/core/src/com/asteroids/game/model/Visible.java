e024e7dc99cecce4adc6290d7a6e6c42e374f584
meogol <avs99.98@gmail.com>
2021-10-18 22:29:52 +0300

    Delete 1-offline-alone directory

--- 1-offline-alone/core/src/com/asteroids/game/model/Visible.java
+++
@@ -1,9 +0,0 @@
-package com.asteroids.game.model;
-
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Polygon;
-
-public interface Visible {
-    Color getColor();
-    Polygon getShape();
-}
